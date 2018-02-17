db6b253e3bb6919ae9580a57a4dcd8ea3ea12cb9
Elizabeth Staveley-Taylor <ucestav@ucl.ac.uk>
2018-02-17 12:44:43 -0500

    updated both documemts

--- ucestav/www/js/appActivity.js
+++ ucestav/www/js/appActivity.js
@@ -1,8 +1,2 @@
   function menuClicked() {  
             alert("You clicked the menu"); 
- } 
-  <ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right" for="hdrbtn">             
-   <li class="mdl-menu__item">About</li>             
-   <li class="mdl-menu__item">Contact</li>             
-   <li class="mdl-menu__item" onclick="menuClicked()">Legal information</li>           
-   </ul> 
